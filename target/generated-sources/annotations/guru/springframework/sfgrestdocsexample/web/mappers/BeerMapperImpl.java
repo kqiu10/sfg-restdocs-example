ddbe681d69b9726315c61e5bf9ef7c28f3494558
kqiu10 <qiukan97@gmail.com>
2021-01-30 17:52:38 -0500

    documenting validation constraints

--- target/generated-sources/annotations/guru/springframework/sfgrestdocsexample/web/mappers/BeerMapperImpl.java
+++ target/generated-sources/annotations/guru/springframework/sfgrestdocsexample/web/mappers/BeerMapperImpl.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2021-01-30T17:14:08-0500",
+    date = "2021-01-30T17:50:00-0500",
     comments = "version: 1.3.0.Final, compiler: javac, environment: Java 11.0.7 (AdoptOpenJDK)"
 )
 @Component
