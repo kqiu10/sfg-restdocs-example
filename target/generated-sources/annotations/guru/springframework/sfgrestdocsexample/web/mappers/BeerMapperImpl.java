f643c99aed5fd2eba4f12100cee1c4703fb51a74
kqiu10 <qiukan97@gmail.com>
2021-01-30 19:39:21 -0500

    documenting Doc generation

--- target/generated-sources/annotations/guru/springframework/sfgrestdocsexample/web/mappers/BeerMapperImpl.java
+++ target/generated-sources/annotations/guru/springframework/sfgrestdocsexample/web/mappers/BeerMapperImpl.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2021-01-30T17:56:50-0500",
+    date = "2021-01-30T18:04:36-0500",
     comments = "version: 1.3.0.Final, compiler: javac, environment: Java 11.0.7 (AdoptOpenJDK)"
 )
 @Component
